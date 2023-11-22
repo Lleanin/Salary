271f68e5b7a72c1457160bacbdcc4029316cfc9f
sssadboy <Fifashf@yandex.ru>
2023-11-22 18:04:05 +0300

    Обработал все страницы ответа api.

--- get_hh_stats.py
+++ get_hh_stats.py
@@ -2,35 +2,39 @@ import requests
 from predict_salary import predict_rub_salary
 
 
-def get_vacancies_hh(page, language, vacancies_for_page):
+def get_vacancies(page, language, vacancies_for_page):
+    area = 1
     payload = {
-        'area': 1,
+        'area': area,
         'text': "Программист {}".format(language),
         'page': page,
-        'per_page': vacancies_for_page,
+        'per_page': vacancies_for_page
     }
     url = 'https://api.hh.ru/vacancies/'
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    vacancies_hh = response.json()
-    return vacancies_hh
+    vacancies = response.json()
+    return vacancies, len(vacancies['items'])
 
 
 def get_hh_statistics(languages):
     vacancies_for_page = 100
-    pages_limit = 19
     salaries = []
-    language_vacancies_hh = {}
+    language_vacancies = {}
     for language in languages:
+        sum = 0
+        vacancies_response = get_vacancies(0,
+                                           language,
+                                           vacancies_for_page)[0]
         vacancies_processed = 0
         salaries_sum = 0
+        pages_limit = vacancies_response['pages']
         for page in range(pages_limit):
-            vacancies_hh = get_vacancies_hh(page,
-                                            language,
-                                            vacancies_for_page)
-            if page >= vacancies_hh["pages"] - 1:
-                break
-            salaries = get_salary_hh(vacancies_hh['items'])
+            vacancies, vacancies_counter = get_vacancies(page,
+                                                         language,
+                                                         vacancies_for_page)
+            sum += vacancies_counter
+            salaries = get_salary(vacancies['items'])
             for salary in salaries:
                 if salary:
                     salaries_sum += salary
@@ -39,23 +43,23 @@ def get_hh_statistics(languages):
             avg_salary = salaries_sum//vacancies_processed
         except ZeroDivisionError:
             avg_salary = 0
-        language_vacancies_hh[language] = {
-            "vacancies_found": vacancies_hh["found"],
+        language_vacancies[language] = {
+            "vacancies_found": sum,
             "vacancies_processed": vacancies_processed,
             "average_salary": avg_salary
         }
-    return language_vacancies_hh
+    return language_vacancies
 
 
-def get_salary_hh(vacancies_hh):
-    language_salaries_hh = []
-    for vacancy in vacancies_hh:
+def get_salary(vacancies):
+    language_salaries = []
+    for vacancy in vacancies:
         salary = vacancy['salary']
         if not salary:
             continue
         elif salary['currency'] != 'RUR':
             continue
         else:
-            language_salaries_hh.append(predict_rub_salary(salary['from'],
-                                                           salary['to']))
-    return language_salaries_hh
+            language_salaries.append(predict_rub_salary(salary['from'],
+                                                        salary['to']))
+    return language_salaries
--- get_sj_stats.py
+++ get_sj_stats.py
@@ -2,7 +2,7 @@ import requests
 from predict_salary import predict_rub_salary
 
 
-def get_vacansies_sj(page, language, sj_key):
+def get_vacansies(page, language, sj_key):
     headers = {
         'X-Api-App-Id': sj_key,
         }
@@ -16,33 +16,33 @@ def get_vacansies_sj(page, language, sj_key):
     url = 'https://api.superjob.ru/2.0/vacancies/'
     response = requests.get(url, headers=headers, params=payload)
     response.raise_for_status()
-    vacancies_sj = response.json()
-    return vacancies_sj
+    vacancies = response.json()
+    return vacancies, len(vacancies['objects'])
 
 
 def get_sj_statistics(languages, sj_key):
     pages_limit = 5
-    language_vacancies_sj = {}
+    language_vacancies = {}
     for language in languages:
+        sum = 0
+        salaries_sum = 0
         salaries = []
         for page in range(pages_limit):
-            vacancies_sj = get_vacansies_sj(page, language, sj_key)
-            if not vacancies_sj['objects']:
-                break
-            for vacancy in vacancies_sj['objects']:
+            vacancies, total_vacancies = get_vacansies(page, language, sj_key)
+            sum += total_vacancies
+            for vacancy in vacancies['objects']:
                 predicted_salary = predict_rub_salary(vacancy['payment_from'],
                                                       vacancy['payment_to'])
                 if predicted_salary:
                     salaries.append(predicted_salary)
-        total_vacancies = vacancies_sj['total']
-        avg_salary = None
+        avg_salary = 0
         if salaries:
-            avg_salary = int(
-                sum(salaries) / len(salaries))
-
-        language_vacancies_sj[language] = {
-            "vacancies_found": total_vacancies,
+            for salary in salaries:
+                salaries_sum += salary
+            avg_salary = salaries_sum // len(salaries)
+        language_vacancies[language] = {
+            "vacancies_found": sum,
             "vacancies_processed": len(salaries),
             "average_salary": avg_salary
         }
-    return language_vacancies_sj
+    return language_vacancies
