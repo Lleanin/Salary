3cb8e5c60150b5e55e7f32d85495b685b2aee47e
sssadboy <Fifashf@yandex.ru>
2023-11-29 17:56:14 +0300

    Подправил работу файла get_sj_stats.py

--- get_sj_stats.py
+++ get_sj_stats.py
@@ -1,11 +1,13 @@
 import requests
 from predict_salary import predict_rub_salary
+from itertools import count
 
 
 def get_vacansies(page, language, sj_key):
     headers = {
         'X-Api-App-Id': sj_key,
         }
+
     payload = {
         'keyword': 'Программист {}'.format(language),
         'town': 'Москва',
@@ -17,32 +19,36 @@ def get_vacansies(page, language, sj_key):
     response = requests.get(url, headers=headers, params=payload)
     response.raise_for_status()
     vacancies = response.json()
-    return vacancies, len(vacancies['objects'])
+
+    return vacancies
 
 
 def get_sj_statistics(languages, sj_key):
-    pages_limit = 5
     language_vacancies = {}
+
     for language in languages:
-        sum = 0
-        salaries_sum = 0
         salaries = []
-        for page in range(pages_limit):
-            vacancies, total_vacancies = get_vacansies(page, language, sj_key)
-            sum += total_vacancies
+        avg_salary = 0
+
+        for page in count(0, 1):
+            vacancies = get_vacansies(page, language, sj_key)
+
             for vacancy in vacancies['objects']:
-                predicted_salary = predict_rub_salary(vacancy['payment_from'],
-                                                      vacancy['payment_to'])
+                if vacancy['currency'] == 'rub':
+                    predicted_salary = predict_rub_salary(vacancy['payment_from'], vacancy['payment_to'])
                 if predicted_salary:
                     salaries.append(predicted_salary)
-        avg_salary = 0
+
+            if not vacancies['more']:
+                break
+
         if salaries:
-            for salary in salaries:
-                salaries_sum += salary
-            avg_salary = salaries_sum // len(salaries)
+            avg_salary = sum(salaries) // len(salaries)
+
         language_vacancies[language] = {
-            "vacancies_found": sum,
+            "vacancies_found": vacancies['total'],
             "vacancies_processed": len(salaries),
             "average_salary": avg_salary
         }
+
     return language_vacancies
